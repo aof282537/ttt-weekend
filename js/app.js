06097e627be8abc5dcf94cedbc786e7adf5e517f
Aof Sangsuwan <warning.2753@email.com>
2022-06-01 11:37:44 -0600

    finish the firs to third step

--- js/app.js
+++ js/app.js
@@ -1,13 +1,18 @@
 /*-------------------------------- Constants --------------------------------*/
 
-
-
 /*---------------------------- Variables (state) ----------------------------*/
-
+let board, turn, winner
+// let firstPlayer = "X"
+// let secondPlayer = "O"
+// let currentPlayer = "O"
 
 
 /*------------------------ Cached Element References ------------------------*/
+const squareEls = document.querySelector(".box")
+const messageEl = document.querySelector("#message")
 
+console.log(squareEls)
+console.log(messageEl)
 
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -15,4 +20,12 @@
 
 
 /*-------------------------------- Functions --------------------------------*/
+init ()
 
+function init() {
+  board = [null, null, null, null, null, null, null, null, null,]
+  turn = 1
+  winner = null
+
+
+}
