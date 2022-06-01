358f47a8bc78b6b64a45f500553b116d63d7a927
Aof Sangsuwan <warning.2753@email.com>
2022-06-01 14:27:00 -0600

    add step4

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 /*-------------------------------- Constants --------------------------------*/
+const winningCombos = [[]]
 
 /*---------------------------- Variables (state) ----------------------------*/
 let board, turn, winner
@@ -31,16 +32,22 @@ function init() {
 
 function render() {
   board.foreach((square, index) => {
-    if (square === 1) {
+    if (square === -1) {
       squareEls[index].textcontent = "O"
-    } else if (square === -1) {
+    } else if (square === 1) {
       squareEls[index].textcontent = "X"
     } else {
       squareEls[index].textcontent = null
     }
   });
 
+
   if (winner === null) {
-    return (turn === 1 )
+    return (turn === 1 ? messageEl.textcontent = "Player 1's turn!!!" : messageEl.textContent = "Players 2's turn!!!") 
+  } else if (winner === "T") {
+    return messageEl.textcontent = "We are tie!!"
+  } else { 
+    return (winner === 1 ? messageEl.textcontent = "Player 1 has won!!!" : messageEl.textcontent = "player 2 has won!!!")
   }
 }
+
